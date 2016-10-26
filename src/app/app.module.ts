cfb66d876a08aa038686f026500d89fc86026283
machariamarigi <machariamarigi@gmail.com>
2016-10-26 09:47:07 +0300

    2nd draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Ng2PaginationModule } from 'ng2-pagination';
+
 import { APP_ROUTING_PROVIDERS, AppRouting } from './app.routing'
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { PlotsService } from './services/plots.service';
 import { SinglePlotComponent } from './single-plot/single-plot.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SplashComponent } from './splash/splash.component';
 import { PlotsComponent } from './plots/plots.component';
+
+import { PlotsService } from './services/plots.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,8 @@ import { PlotsComponent } from './plots/plots.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRouting
+    AppRouting,
+    Ng2PaginationModule
   ],
   providers: [
     PlotsService,
